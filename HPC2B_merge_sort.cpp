e49c0c87f7a79257d076438751bc6eba67f0e19c
Hritvikk Gupta <67199772+Hritvikk@users.noreply.github.com>
2023-05-21 11:56:00 +0530

    Update HPC2B_merge_sort.cpp

--- HPC2B_merge_sort.cpp
+++ HPC2B_merge_sort.cpp
@@ -46,7 +46,7 @@ void merge(int arr[],int low,int mid,int high){
 
 void mergeSort(int arr[],int low, int high){
     if(low<high){
-        int mid=low+(high-low)/2;
+        int mid=(low+high)/2;
 
         mergeSort(arr,low,mid);
         mergeSort(arr,mid+1,high);
